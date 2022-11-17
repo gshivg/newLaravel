3cccf0175dc622c47eb7f2cea7afc96ac6a207f6
Shivansh Gupta <shivanshkush254@gmail.com>
2022-11-17 07:37:30 +0530

    Added username to user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,32 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/ikqra03zdnggljdu5vv0" class="rounded-circle" style="height: 150px;">
+            <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/ikqra03zdnggljdu5vv0"
+                class="rounded-circle" style="height: 150px;">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freeCodeCamp</h1></div>
+            <div>
+                <h1>freeCodeCamp</h1>
+            </div>
             <div class="d-flex pt-3">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> following</div>
                 <div class="pr-5"><strong>212</strong> followers</div>
             </div>
-            <div class="pt-3">freeCodeCamp.org</div>
+            <div class="pt-3"><strong>freeCodeCamp.org</strong></div>
+            <div>We are a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="freeCodeCamp.org">freeCodeCamp.org</div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://png.pngtree.com/element_our/20200609/ourlarge/pngtree-technology-square-background-image_2234814.jpg">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FYCHjNaHVfrznRO66le-L3UJEx5HsnIAZQ&usqp=CAU">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLn3zuUmstmpWcXfgEz98-X61q9Mx0nmYHTw&usqp=CAU">
         </div>
     </div>
 </div>
