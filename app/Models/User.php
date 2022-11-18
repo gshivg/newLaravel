a5005b96c7f22b1bfe3b329cedc7bbdb7139dd09
Shivansh Gupta <shivanshkush254@gmail.com>
2022-11-18 20:11:08 +0530

    require auth before posting

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,12 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 
-    public function profile(){
+    public function profile()
+    {
         return $this->hasOne(Profile::class);
     }
 
-    public function posts(){
-        return $this->hasMany(Post::class);
-    }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,20 +7,18 @@
 
         <div class="row">
             <div class="col-8 offset-2">
+
                 <div class="row">
                     <h1>Add New Post</h1>
                 </div>
 
                 <div class="row mb-3">
                     <label for="caption" class="col-md-4 col-form-label">{{ __('Post Caption') }}</label>
-
                     <input id="caption" type="text" name="caption"
                         class="form-control @error('name') is-invalid @enderror" caption="caption"
                         value="{{ old('caption') }}" autocomplete="caption" autofocus>
-                    
                     @error('caption')
-                    <!-- TODO: fix the error messages not showing -->
-                    <div class="invalid-feedback" role="alert">
+                    <div class="invalid-feedback d-block"  role="alert">
                         <strong>{{ $message }}</strong>
                     </div>
                     @enderror
@@ -32,16 +30,16 @@
                     <input type="file" class="form-control-file" id="image" name="image">
 
                     @error('image')
-                    <span class="invalid-feedback" role="alert">
+                    <span class="invalid-feedback d-block" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                     @enderror
-
                 </div>
 
                 <div class="row pt-4">
                     <button class="btn btn-primary">Add new Post</button>
                 </div>
+
             </div>
         </div>
     </form>
