16527d6b1fb4be593b972653c6a6124bf32ff0bc
Shivansh Gupta <shivanshkush254@gmail.com>
2022-11-20 19:39:48 +0530

    Automatically gerating profile using model events

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,8 +5,28 @@
             <img src="/storage/{{ $post->image }}" class="w-100" />
         </div>
         <div class="col-4">
-            <h3>{{ $post->user->username }}</h3>
-            <p>{{ $post->caption }}</p>
+            <div class="d-flex align-items-center">
+                <div>
+                    <img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle w-100" style="max-width: 50px;">
+                </div>
+                <div class="pl-2">
+                    <div class="font-weight-bold">
+                        <a href="/profile/{{$post->user->id}}" style="text-decoration: none;">
+                            <span class="text-dark">{{ $post->user->username }}</span>
+                        </a> •
+                        <a href="#" style="text-decoration: none;" >Follow</a>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p>
+                <span class="font-weight-bold">
+                    <a href="/profile/{{$post->user->id}}" style="text-decoration: none;">
+                        <span class="text-dark" >{{ $post->user->username }}</span>
+                    </a>
+                </span> 
+                    {{ $post->caption }}
+            </p>
         </div>
     </div>
 </div>
